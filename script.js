d651f0f65a29036896437fbbab514d4cfb9219c5
Sych Oleh <sicholeg1@gmail.com>
2026-05-17 00:22:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const todoBtn = document.getElementById('add-btn');
+const todoList = document.getElementById('todo-list');
+const todoInput = document.getElementById('todo-input');
+
+
+
+function showData() {
+  todoList.innerHTML = localStorage.getItem('todos') || '';
+};
+showData()
+
+todoBtn.addEventListener('click', () => {
+  if (todoInput.value.trim() !== '') {
+    const newItem = document.createElement('li')
+    newItem.innerHTML = `
+    <input type='checkbox'></input>
+    <span>${todoInput.value}</span>
+    <button class='delete-btn' id='delete-btn'>X</button>
+    `
+    newItem.className = 'text-white flex justify-between p-2 px-4 bg-[#563669] border-[#60496e] border-1 rounded';
+
+    todoList.appendChild(newItem);
+    saveData();
+
+    todoInput.value = '';
+  }
+});
+
+todoList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete-btn')) {
+    e.target.closest('li').remove()
+    saveData()
+  };
+});
+
+function saveData() {
+  localStorage.setItem('todos', todoList.innerHTML);
+}
