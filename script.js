6dc2907dff4c9d1d949c62eac91c34287261a587
Sych Oleh <sicholeg1@gmail.com>
2026-05-17 12:12:01 +0300

    minor change

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ todoBtn.addEventListener('click', () => {
     newItem.innerHTML = `
     <input type='checkbox'></input>
     <span>${todoInput.value}</span>
-    <button class='delete-btn' id='delete-btn'>X</button>
+    <button class='delete-btn'>X</button>
     `
     newItem.className = 'text-white flex justify-between p-2 px-4 bg-[#563669] border-[#60496e] border-1 rounded';
 
